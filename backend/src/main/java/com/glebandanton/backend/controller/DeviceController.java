9eabb9e858f82f545b0f6723b6f3afce27b9ddf9
Dark Phoenix <anton2004.12.24@gmail.com>
2025-07-07 02:07:11 +0300

    Добавил DeviceController по пути /api/products. Выдаёт все данные из таблицы

--- backend/src/main/java/com/glebandanton/backend/controller/DeviceController.java
+++ backend/src/main/java/com/glebandanton/backend/controller/DeviceController.java
@@ -1,20 +1,30 @@
 package com.glebandanton.backend.controller;
 
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.glebandanton.backend.model.Device;
+import com.glebandanton.backend.service.DeviceService;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@AllArgsConstructor
-@Controller
+import java.util.List;
+
+@Data
+@RestController
+@RequestMapping("/api")
 public class DeviceController {
+    private final DeviceService deviceService;
 
-    @GetMapping("/")
-    public String index() {
-        return "redirect:/index.html";
+    @PostMapping("/products")
+    public ResponseEntity<?> getAllProducts() {
+        try {
+            List<Device> devices = deviceService.readAllDevices();
+            return ResponseEntity.ok(devices);
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error to get devices" + e.getMessage());
+        }
     }
-
 }
-//    @GetMapping("/")
-//    public ResponseEntity<List<Device>> readAllDevices() {
-//        return new ResponseEntity<>(deviceService.readAllDevices(), HttpStatus.OK);
-//    }
---
+++ backend/src/main/java/com/glebandanton/backend/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.glebandanton.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/index.html";
+    }
+}
+//    @GetMapping("/")
+//    public ResponseEntity<List<Device>> readAllDevices() {
+//        return new ResponseEntity<>(deviceService.readAllDevices(), HttpStatus.OK);
+//    }
