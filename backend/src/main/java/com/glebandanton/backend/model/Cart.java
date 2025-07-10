b003c258eef466b01646183e263d6b095649712c
Dark Phoenix <anton2004.12.24@gmail.com>
2025-07-10 12:13:19 +0300

    Пофиксил баг с корзиной

--- backend/src/main/java/com/glebandanton/backend/model/Cart.java
+++ backend/src/main/java/com/glebandanton/backend/model/Cart.java
@@ -1,10 +1,10 @@
-package com.glebandanton.backend.model;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "cart")
-public class Cart {
-    
-}
+//package com.glebandanton.backend.model;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//
+//@Entity
+//@Table(name = "cart")
+//public class Cart {
+//
+//}
